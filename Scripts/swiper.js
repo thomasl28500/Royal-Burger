5c7960b5a2cda1cf5ff3a11940bfbabda1b9529d
Thomas <112702448+thomasl28500@users.noreply.github.com>
2025-12-28 14:42:16 +0100

    partenaires + structure footer

--- Scripts/swiper.js
+++ Scripts/swiper.js
@@ -1,11 +1,10 @@
 var swiper = new Swiper(".slider-wrapper", {
     spaceBetween: 30,
-    loop: true,
     grabCursor: true,
     speed: 1000,
-    // autoplay: {
-    //     delay: 2000,
-    // },
+    autoplay: {
+        delay: 2000,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
