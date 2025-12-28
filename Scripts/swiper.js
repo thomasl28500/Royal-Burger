feea2e3cf4600e81d50260f69fbcd0d4317f243e
Thomas <112702448+thomasl28500@users.noreply.github.com>
2025-12-28 13:29:44 +0100

    Swiper + fin CSS Avis

---
+++ Scripts/swiper.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".slider-wrapper", {
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    speed: 1000,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+    },
+});
